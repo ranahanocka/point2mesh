e3d39fe5cf5bc366579bd13bdf7d2f5633a65e7d
rana <github@hanocka.com>
2020-05-25 05:21:46 +0300

    clean

--- scripts/examples/noisy_ankylosaurus.sh
+++
@@ -1,8 +0,0 @@
-python main.py --input-pc ./data/noisy_ankylosaurus.ply \
---initial-mesh ./data/noisy_ankylosaurus_initmesh.obj \
---save-path ./checkpoints/noisy_ankylosaurus2_long \
---samples 60000 \
---begin-samples 40000 \
---max-faces 15000 \
---pools 0.1 0.2 0.3 0.4 \
---iterations 6000
